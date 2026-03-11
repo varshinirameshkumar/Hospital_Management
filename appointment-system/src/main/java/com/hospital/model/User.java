0c5595cd9e7b215ee2fe4301d66713465b2283a1
varshinirameshkumar <146193556+varshinirameshkumar@users.noreply.github.com>
2026-03-12 00:47:26 +0530

    chore: add Dockerfile for Render deployment

--- appointment-system/src/main/java/com/hospital/model/User.java
+++ appointment-system/src/main/java/com/hospital/model/User.java
@@ -1,51 +1,21 @@
 package com.hospital.model;
 
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
+@Data 
 @Document(collection = "users")
 public class User {
     @Id
     private String id;
     private String name;
     private String email;
-    private String role;
+    private String role; 
     private String specialization;
-
-    public User() {}
-
-    public String getId() { 
-        return id; 
-    }
-    public void setId(String id) { 
-        this.id = id; 
-    }
-
-    public String getName() { 
-        return name; 
-    }
-    public void setName(String name) { 
-        this.name = name; 
-    }
-
-    public String getEmail() { 
-        return email; 
-    }
-    public void setEmail(String email) { 
-        this.email = email; 
-    }
-
-    public String getRole() { 
-        return role; 
-    }
-    public void setRole(String role) { 
-        this.role = role; 
-    }
-
-    public String getSpecialization() { 
-        return specialization; 
-    }
-    public void setSpecialization(String specialization) { 
-        this.specialization = specialization; 
-    }
+    
+    private List<Slot> availableSlots; 
 }
